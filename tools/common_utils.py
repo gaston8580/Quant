7de9a53557fcb2b34532001e3b8edc86f96335af
chenxin <chenxin7@mychery.com>
2024-09-06 19:37:20 +0800

    add code for dist train

---
+++ tools/common_utils.py
@@ -0,0 +1,135 @@
+# Motion Transformer (MTR): https://arxiv.org/abs/2209.13508
+# Published at NeurIPS 2022
+
+
+import numpy as np
+import torch
+import logging
+import torch.distributed as dist
+import random
+import os
+import subprocess
+import pickle
+import shutil
+from torch import Tensor
+
+
+def check_numpy_to_torch(x):
+    if isinstance(x, np.ndarray):
+        return torch.from_numpy(x).float(), True
+    return x, False
+
+
+def create_logger(log_file=None, rank=0, log_level=logging.INFO):
+    logger = logging.getLogger(__name__)
+    logger.setLevel(log_level if rank == 0 else 'ERROR')
+    formatter = logging.Formatter('%(asctime)s  %(levelname)5s  %(message)s')
+    console = logging.StreamHandler()
+    console.setLevel(log_level if rank == 0 else 'ERROR')
+    console.setFormatter(formatter)
+    logger.addHandler(console)
+    if log_file is not None:
+        file_handler = logging.FileHandler(filename=log_file)
+        file_handler.setLevel(log_level if rank == 0 else 'ERROR')
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+
+    logger.propagate = False
+    return logger
+
+
+def get_dist_info(return_gpu_per_machine=False):
+    if torch.__version__ < '1.0':
+        initialized = dist._initialized
+    else:
+        if dist.is_available():
+            initialized = dist.is_initialized()
+        else:
+            initialized = False
+    if initialized:
+        rank = dist.get_rank()
+        world_size = dist.get_world_size()
+    else:
+        rank = 0
+        world_size = 1
+
+    if return_gpu_per_machine:
+        gpu_per_machine = torch.cuda.device_count()
+        return rank, world_size, gpu_per_machine
+
+    return rank, world_size
+
+
+def set_random_seed(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+
+def init_dist_slurm(tcp_port, local_rank, backend='nccl'):
+    """
+    modified from https://github.com/open-mmlab/mmdetection
+    Args:
+        tcp_port:
+        backend:
+
+    Returns:
+
+    """
+    proc_id = int(os.environ['SLURM_PROCID'])
+    ntasks = int(os.environ['SLURM_NTASKS'])
+    node_list = os.environ['SLURM_NODELIST']
+    num_gpus = torch.cuda.device_count()
+    torch.cuda.set_device(proc_id % num_gpus)
+    addr = subprocess.getoutput('scontrol show hostname {} | head -n1'.format(node_list))
+    os.environ['MASTER_PORT'] = str(tcp_port)
+    os.environ['MASTER_ADDR'] = addr
+    os.environ['WORLD_SIZE'] = str(ntasks)
+    os.environ['RANK'] = str(proc_id)
+    dist.init_process_group(backend=backend)
+
+    total_gpus = dist.get_world_size()
+    rank = dist.get_rank()
+    return total_gpus, rank
+
+
+def init_dist_pytorch(tcp_port, local_rank, backend='nccl'):
+    # if mp.get_start_method(allow_none=True) is None:
+    #     mp.set_start_method('spawn')
+    num_gpus = torch.cuda.device_count()
+    torch.cuda.set_device(local_rank % num_gpus)
+
+    dist.init_process_group(
+        backend=backend,
+        # init_method='tcp://127.0.0.1:%d' % tcp_port,
+        # rank=local_rank,
+        # world_size=num_gpus
+    )
+    rank = dist.get_rank()
+    return num_gpus, rank
+
+
+def merge_results_dist(result_part, size, tmpdir):
+    rank, world_size = get_dist_info()
+    os.makedirs(tmpdir, exist_ok=True)
+
+    dist.barrier()
+    pickle.dump(result_part, open(os.path.join(tmpdir, 'result_part_{}.pkl'.format(rank)), 'wb'))
+    dist.barrier()
+
+    if rank != 0:
+        return None
+
+    part_list = []
+    for i in range(world_size):
+        part_file = os.path.join(tmpdir, 'result_part_{}.pkl'.format(i))
+        part_list.append(pickle.load(open(part_file, 'rb')))
+
+    ordered_results = []
+    for res in zip(*part_list):
+        ordered_results.extend(list(res))
+    ordered_results = ordered_results[:size]
+    shutil.rmtree(tmpdir)
+    return ordered_results
--- train.py
+++ train.py
@@ -1,45 +1,40 @@
-import torch
-from torch import nn
+import torch.nn as nn
 from Alexnet import AlexNet
+from tools import common_utils
 from torch.optim import lr_scheduler
-import os, time
-
 from torchvision import transforms
 from torchvision.datasets import ImageFolder
 from torch.utils.data import DataLoader
 import torch.distributed as dist
-
 import matplotlib.pyplot as plt
+import torch, os, time, argparse, warnings, datetime
 
-import warnings
 warnings.filterwarnings('ignore')
 
 ROOT_TRAIN = r'/data/sfs_turbo/perception/animals/train'
 ROOT_TEST = r'/data/sfs_turbo/perception/animals/val'
 
-# 将图像的像素值归一化到[-1, 1]
-normalize = transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
 
-# 训练集
-train_transform = transforms.Compose([
-    transforms.Resize((224, 224)),
-    transforms.RandomVerticalFlip(),  # 随机垂直全展, 增强数据
-    transforms.ToTensor(),
-    normalize
-])
+def build_dataloader():
+    # 将图像的像素值归一化到[-1, 1]
+    normalize = transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
 
-# 验证集
-val_transform = transforms.Compose([
-    transforms.Resize((224, 224)),
-    transforms.ToTensor(),
-    normalize
-])
+    train_transform = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.RandomVerticalFlip(),  # 随机垂直全展, 增强数据
+        transforms.ToTensor(),
+        normalize])
 
-train_dataset = ImageFolder(ROOT_TRAIN, transform=train_transform)
-val_dataset = ImageFolder(ROOT_TEST, transform=val_transform)
+    val_transform = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        normalize])
 
-train_dataloader = DataLoader(train_dataset, batch_size=256, shuffle=True)
-val_dataloader = DataLoader(val_dataset, batch_size=256, shuffle=True)
+    train_dataset = ImageFolder(ROOT_TRAIN, transform=train_transform)
+    val_dataset = ImageFolder(ROOT_TEST, transform=val_transform)
+
+    train_dataloader = DataLoader(train_dataset, batch_size=256, shuffle=True)
+    val_dataloader = DataLoader(val_dataset, batch_size=256, shuffle=True)
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model = AlexNet().cuda()
@@ -54,7 +49,7 @@ optimizer = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.9)  # SGD随
 lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.5)
 
 # 定义训练函数
-def train(dataloader, model, loss_fn, optimizer):
+def train_one_epoch(dataloader, model, loss_fn, optimizer):
     loss, current, n = 0.0, 0.0, 0
     for batch, (x, y) in enumerate(dataloader):
         image, y = x.to(device), y.to(device)
@@ -119,40 +114,89 @@ def matplot_acc(train_acc, val_acc, folder):
     plt.savefig(f'{folder}/acc.png')
 
 
-# 开始训练
-loss_train = []
-acc_train = []
-loss_val = []
-acc_val = []
+def train_model():
+    args = parse_config()
+    if args.launcher == 'none':
+        dist_train = False
+        total_gpus = 1
+        args.without_sync_bn = True
+    else:
+        dist_train = True
+        if args.local_rank is None:
+            args.local_rank = int(os.environ.get('LOCAL_RANK', '0'))
+        total_gpus, args.local_rank = getattr(common_utils, f'init_dist_{args.launcher}') \
+                                        (args.tcp_port, args.local_rank, backend='nccl')
+    
+    details = datetime.datetime.now().strftime('%Y%m%d-%H%M%S')
+    log_file = os.path.join(args.output_dir, f'log_train_{details}.txt')
+    logger = common_utils.create_logger(log_file, rank=args.local_rank)
 
-folder = 'outputs'
-epoch = 64
-min_acc = 0
-for t in range(epoch):
-    start = time.time()
-    print(f"epoch {t + 1} / {epoch}")
-    train_loss, train_acc = train(train_dataloader, model, loss_fn, optimizer)
-    val_loss, val_acc = val(val_dataloader, model, loss_fn)
+    # log to file
+    logger.info('**********************Start logging**********************')
+    logger.info(f'CUDA DEVICES = {[i for i in range(torch.cuda.device_count())]}')
+    if dist_train:
+        logger.info('total_batch_size: %d' % (total_gpus * args.batch_size))
+    for key, val in vars(args).items():
+        logger.info('{:16} {}'.format(key, val))
 
-    loss_train.append(train_loss)
-    acc_train.append(train_acc)
-    loss_val.append(val_loss)
-    acc_val.append(val_acc)
+    loss_train, acc_train, loss_val, acc_val = [], [], [], []
 
-    lr_scheduler.step()
+    folder = 'outputs'
+    epoch = 128
+    max_acc = 0
+    for t in range(epoch):
+        start = time.time()
+        print(f"epoch {t + 1} / {epoch}")
+        train_loss, train_acc = train_one_epoch(train_dataloader, model, loss_fn, optimizer)
+        val_loss, val_acc = val(val_dataloader, model, loss_fn)
 
-    # 保存最好的模型权重
-    if val_acc > min_acc:
-        if not os.path.exists(folder):
-            os.mkdir(folder)
-        min_acc = val_acc
-        print(f"save best model, epoch {t + 1}")
-        torch.save(model.state_dict(), f'{folder}/best_model.pth')
+        loss_train.append(train_loss)
+        acc_train.append(train_acc)
+        loss_val.append(val_loss)
+        acc_val.append(val_acc)
 
-    if t == epoch - 1:
-        torch.save(model.state_dict(), f'{folder}/latest_model.pth')
-    print(f"epoch {t + 1} done in {time.time() - start:.3f} seconds\n-----------------------")
+        lr_scheduler.step()
 
-matplot_loss(loss_train, loss_val, folder)
-matplot_acc(acc_train, acc_val, folder)
-print('Training Done!')
+        # 保存最好的模型权重
+        if val_acc > max_acc:
+            if not os.path.exists(folder):
+                os.mkdir(folder)
+            max_acc = val_acc
+            print(f"save best model, epoch {t + 1}")
+            torch.save(model.state_dict(), f'{folder}/best_model.pth')
+
+        if t == epoch - 1:
+            torch.save(model.state_dict(), f'{folder}/latest_model.pth')
+        print(f"epoch {t + 1} done in {time.time() - start:.3f} seconds\n-----------------------")
+
+    matplot_loss(loss_train, loss_val, folder)
+    matplot_acc(acc_train, acc_val, folder)
+    print('Training Done!')
+
+
+def parse_config():
+    parser = argparse.ArgumentParser(description='arg parser')
+    parser.add_argument('--batch_size', type=int, default=256, required=False, help='batch size for training')
+    parser.add_argument('--epochs', type=int, default=100, required=False, help='number of epochs to train for')
+    parser.add_argument('--workers', type=int, default=10, help='number of workers for dataloader')
+    parser.add_argument('--output_dir', default='outputs', help='dir for saving ckpts and log files')
+    parser.add_argument('--ckpt', type=str, default=None, help='checkpoint to start from')
+    parser.add_argument('--pretrained_model', type=str, default=None, help='pretrained_model')
+    parser.add_argument('--launcher', choices=['none', 'pytorch', 'slurm'], default='pytorch')
+    parser.add_argument('--tcp_port', type=int, default=18888, help='tcp port for distrbuted training')
+    parser.add_argument('--without_sync_bn', action='store_true', default=False, help='whether to use sync bn')
+    parser.add_argument('--fix_random_seed', action='store_true', default=False, help='')
+    parser.add_argument('--ckpt_save_interval', type=int, default=2, help='number of training epochs')
+    parser.add_argument('--local_rank', type=int, default=None, help='local rank for distributed training')
+    parser.add_argument('--max_ckpt_save_num', type=int, default=5, help='max number of saved checkpoint')
+    parser.add_argument('--merge_all_iters_to_one_epoch', action='store_true', default=False, help='')
+    parser.add_argument('--start_epoch', type=int, default=0, help='')
+    parser.add_argument('--not_eval_with_train', action='store_true', default=False, help='')
+    parser.add_argument('--logger_iter_interval', type=int, default=50, help='')
+    parser.add_argument('--ckpt_save_time_interval', type=int, default=300, help='in terms of seconds')
+    parser.add_argument('--add_worker_init_fn', action='store_true', default=False, help='')
+    args = parser.parse_args()
+    return args
+
+if __name__ == '__main__':
+    train_model()
